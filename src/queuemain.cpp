849169879d3d97758d80dd19622628ef236770d5
Prashesh Adhikari <aprashesh@gmail.com>
2024-05-22 10:51:26 +0545

    aba huncha hola

--- src/queuemain.cpp
+++ src/queuemain.cpp
@@ -23,4 +23,24 @@ int main()
     cout<<"The rear element is :"<<data<<endl;
     queue->traverse();
 
+
+    Queue *queue2 = new LinkedListQueue();
+    int data;
+    if(queue2->isEmpty())
+    {
+        cout<<"The queue is empty "<<endl;
+
+    }
+    queue2->enQueue(2);
+    queue2->enQueue(2);
+    queue2->enQueue(2);
+    queue2->enQueue(2);
+    queue2->deQueue(data);
+    cout<<"The dequeued element is :"<<data<<endl;
+    queue2->front(data);
+    cout<<"The front element is :"<<data<<endl;
+    queue2->rear(data);
+    cout<<"The rear element is :"<<data<<endl;
+    queue2->traverse();2
+
 }
